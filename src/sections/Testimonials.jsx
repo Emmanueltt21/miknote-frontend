40834b895431ac4f7a3ab04f373b982c4425146a
Emmanuel Taah <81640127+Emmanueltt21@users.noreply.github.com>
2024-04-26 16:32:50 +0100

    section profile update
    section profile update

--- src/sections/Testimonials.jsx
+++ src/sections/Testimonials.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import { clients } from '../exports'
+import ClientGrid from '../components/ClientGrid'
 
 const Testimonials = () => {
   return (
-    <div>Testimonials</div>
+   <section id='testimonials' className='w-full flex flex-col gap-5 h-fit lg:p-20'>
+    <p className='text-center text-xl'> Testimonials</p>
+    <h1 className='text-green-600 font-bold text-6xl text-center leading-[68px]'>What Clients Says</h1>
+    <p className='text-slate-950 text-2xl text-center'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nemo placeat atque dignissimos, provident totam incidunt perspiciatis suscipit cum iusto, reiciendis ullam 
+     aperiam cupiditate magnam minima <br /> mollitia omnis temporibus sed repellendus.</p>
+     <div className=' flex justify-center flex-wrap mt-5 w-full gap-6'>
+      {clients.map((client) =>(
+        <div key={client.name} className='w-80'>
+          {<ClientGrid {...client} />}
+          </div>
+      ))}
+     </div>
+   </section>
   )
 }
 
